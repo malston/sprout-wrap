70dffdfa9e99fe5877ec6b0649ba6b011135d5fd
Mark Alston <mark_alston@yahoo.com>
2014-11-24 01:11:45 -0700

    A few more customizations to my env

--- site-cookbooks/malston/recipes/bash_it.rb
+++ site-cookbooks/malston/recipes/bash_it.rb
@@ -1,1 +1,4 @@
-sprout_base_bash_it_custom_plugin 'bash_it/malston.bash'
+sprout_base_bash_it_custom_plugin 'bash_it/aliases.bash'
+
+
+node.default['sprout']['base']['bash_it']['theme']='mbriggs'
---
+++ site-cookbooks/malston/templates/default/bash_it/aliases.bash
@@ -0,0 +1,4 @@
+alias be='bundle exec'
+alias gs='git status'
+
+export GEMDEV=1
