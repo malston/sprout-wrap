c3214ea8e1e00e4cd2ed622b0afdb9df66c37ffa
Mark Alston <mark_alston@yahoo.com>
2014-11-29 15:21:44 -0700

    Customizations to bash_it

--- site-cookbooks/malston/recipes/bash_it.rb
+++ site-cookbooks/malston/recipes/bash_it.rb
@@ -1,5 +1,27 @@
-sprout_base_bash_it_custom_plugin 'bash_it/aliases.bash'
+# Aliases
+sprout_base_bash_it_enable_feature 'aliases/bundler'
+sprout_base_bash_it_enable_feature 'aliases/git'
+sprout_base_bash_it_enable_feature 'aliases/osx'
+sprout_base_bash_it_enable_feature 'aliases/vagrant'
 
+# Plugins
+sprout_base_bash_it_enable_feature 'plugins/git'
+sprout_base_bash_it_enable_feature 'plugins/tmux'
+sprout_base_bash_it_enable_feature 'plugins/tmuxinator'
+sprout_base_bash_it_enable_feature 'plugins/vagrant'
+
+# Custom
+sprout_base_bash_it_custom_plugin 'bash_it/aliases.bash'
+sprout_base_bash_it_custom_plugin 'bash_it/functions.bash'
 sprout_base_bash_it_custom_plugin 'bash_it/path.bash'
 
+# Theme
 node.default['bash_it']['theme']='bobby'
+
+# Customizing ~/.bash_profile
+template node['bash_it']['bashrc_path'] do
+  source "bash_it/bashrc.erb"
+  cookbook 'malston'
+  owner node['current_user']
+  mode "0777"
+end
--- site-cookbooks/malston/templates/default/bash_it/aliases.bash
+++ site-cookbooks/malston/templates/default/bash_it/aliases.bash
@@ -1,4 +1,7 @@
-alias be='bundle exec'
-alias gs='git status'
-
-export GEMDEV=1
+# Taken from https://github.com/zpatten/cc-chef-repo/blob/master/README.md
+alias cbdev='export CBDEV=1'         # turn chef cookbook development on
+alias nocbdev='export CBDEV=0'       # turn chef cookbook development off
+alias gemdev='export GEMDEV=1'       # turn gem development on
+alias nogemdev='export GEMDEV=0'     # turn gem development off
+alias verbose='export VERBOSE=1'     # turn verbose on
+alias noverbose='export VERBOSE=0'   # turn verbose off
